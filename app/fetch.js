910c20ae2da48135d54e6f80bbbd2296755b7108
Shaun Friend <Semfry>
2025-01-27 12:58:46 +0000

    converted to Javascript

---
+++ app/fetch.js
@@ -0,0 +1,7 @@
+import RequestManager from '@ember-data/request';
+import Fetch from '@ember-data/request/fetch';
+
+const fetch = new RequestManager();
+// manager.use([Fetch]);
+
+export default fetch;
