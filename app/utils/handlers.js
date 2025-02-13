48c54bbf7491a0cf2599a8406f83b5ef63dd4497
Shaun Friend <Semfry>
2025-02-13 13:25:56 +0000

    URL Fix

--- app/utils/handlers.js
+++ app/utils/handlers.js
@@ -2,7 +2,7 @@ export const modelHandler = {
   request(context, next) {
     const { request } = context;
     const updatedRequest = Object.assign({}, request, {
-      url: 'http://localhost:8000/api/0.1/contacts',
+      url: 'http://localhost:8000/api/0.1/contacts/',
     });
 
     return next(updatedRequest);
