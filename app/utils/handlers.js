a417b561b976d97d697bb3fd7b03680e26225e18
Shaun Friend <Semfry>
2025-02-13 13:39:49 +0000

    removing typescript formatting

--- app/utils/handlers.js
+++ app/utils/handlers.js
@@ -1,7 +1,7 @@
 export const modelHandler = {
   request(context, next) {
-    const { request } = context;
-    const updatedRequest = Object.assign({}, request, {
+    let { request } = context;
+    let updatedRequest = Object.assign({}, request, {
       url: 'http://localhost:8000/api/0.1/contacts/',
     });
 
